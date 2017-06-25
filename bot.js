191f2aec214255da6b1e6bc656986c17ec2dd266
Your name <solisjesus700@gmail.com>
2017-06-25 09:59:24 -0500

    added bot id file, added command to retrieve user id

--- bot.js
+++ bot.js
@@ -9,22 +9,45 @@ function respond() {
 
   var text = request.text;  
   var textLCase = text.toLowerCase();
+  var user_id = request.user_id;
+
+  if (user_id == 0 || user_id == getBotUserId() || user_id == getPurpleGort()) return; // 0 is GroupMe's user id
 
   if(request.text && text.indexOf("helix") !== -1) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage("output");
     this.res.end();
   } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
+  
+  if (textLCase.indexOf("gort show my id") == 0) { // && (user_id == "39808536") && user_id == "35415238"  { //to obtain user ID
+    if(user_id == "39808536") {
+       sendText("Your user ID is: -1/12");
+       return;
+    }
+    else if (user_id == "35415238") {
+		sendText("Your user ID is: 0");
+        return;
+  }
+    else if (user_id == "32910049") {
+		sendText("Your user ID is: 3.14159265358979");
+        return;
+  }
+    else {
+      sendText("Your user ID is: " + user_id)
+      return;
+    }
+  }
+  
 }
 
-function postMessage() {
+function postMessage(output) {
   var botResponse, options, body, botReq;
 
-  botResponse = "output";
+  botResponse = output;
 
   options = {
     hostname: 'api.groupme.com',
---
+++ bot_id.js
@@ -0,0 +1,18 @@
+//var botId = "545cec4d7d9259f2783f34dce3";
+var groupId = "27486707";
+var botUserId = 394929;
+var purpleGort = 395600;
+
+// DO NOT CHANGE THE LINES ABOVE
+
+function getBotId() {
+  return botId;
+}
+
+function getBotUserId() {
+  return botUserId;
+}
+
+function getPurpleGort() {
+  return purpleGort;
+}
