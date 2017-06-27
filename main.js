3bdba8de814fbf709b83e474ee6092d47a3735cb
Your name <solisjesus700@gmail.com>
2017-06-27 14:05:14 -0500

    no longer sends bot user id after every message

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function respond() {
   var textLCase = text.toLowerCase();
   var user_id = request.user_id;
 
-  sendText(getBotUserId());
+  //sendText(getBotUserId());
 
   if (user_id == 0 || user_id == getBotUserId() || user_id == getPurpleGort()) return; // 0 is GroupMe's user id
 
