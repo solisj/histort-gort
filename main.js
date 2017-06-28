e3d89181b13d378809e7d91ce63aa4d2aa452c2a
Your name <solisjesus700@gmail.com>
2017-06-28 14:57:01 -0500

    added name variable, readded containsGort function

--- main.js
+++ main.js
@@ -20,16 +20,17 @@ function respond() {
   var text = request.text;
   var textLCase = text.toLowerCase();
   var user_id = request.user_id;
+  var name = request.name;
 
   //sendText(getBotUserId());
 
   if (user_id == 0 || user_id == getBotUserId() || user_id == getPurpleGort()) return; // 0 is GroupMe's user id
 
-  /*if (containsGort(name)) {
+  if (containsGort(name)) {
     sendText("No, fuck you");
     sendText("Change your nickname");
     return;
-  }*/
+  }
 
   if(request.text && text.indexOf("helix") !== -1) {
     this.res.writeHead(200);
