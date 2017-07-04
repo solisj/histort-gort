de4c6483820de8f54032d584189eb59fda387ec4
Your name <solisjesus700@gmail.com>
2017-07-04 12:46:48 -0500

    testing parts of urban dictionary functionality

--- main.js
+++ main.js
@@ -91,6 +91,7 @@ function respond() {
   if (text.substr(0,"gort define ".length) == "gort define " && text !== "gort define ") {
     var word = text.substr("gort define ".length);
     sendText(urbanDefine(word));
+    sendText("text");
     return;
   }
 
